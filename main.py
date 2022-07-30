28099d0358ee622244e368999f1513150541a146
AJV6812 <82735267+AJV6812@users.noreply.github.com>
2022-07-30 16:15:02 +1000

    Update main.py

--- main.py
+++ main.py
@@ -440,8 +440,8 @@ async def help(ctx):
             
     embeds = [disnake.Embed(title="Help",description = """This bot has been designed to mimic the notification system of the Twilight Wars web app for people who are unable to get notifications. Please message Al Vergis if you require any assistance""",colour=disnake.Colour.blue()) for x in range(3)]
     embeds[0].add_field(name="Normal Notify", value = "To get started type '/notify ' then paste the URL of the game that you want to be monitored. A select menu will appear which will ask you to pick your TW Username. After you have chosen your username, the bot will confirm that the notification has been saved.",inline=False)
-    embeds[0].add_field(name="Bulk Notify", value="To add multiple notifications, use '/bulknotify' followed by the URL of a game. If you click the end of the message it will give you the option to add another field to the command, which you can paste the next URL into. You can add between 1 and 25 games using the method.")
-    embeds[0].add_field(name="Quick Notify", value="To all of your active public games, use '/quicknotify'. The first time you use this, you may be prompted to use /setdefault. Once you have done this, then the bot will search through all public games and notify you when any of them are waiting for you.")
+    embeds[0].add_field(name="Bulk Notify", value="To add multiple notifications, use '/bulknotify' followed by the URL of a game. If you click the end of the message it will give you the option to add another field to the command, which you can paste the next URL into. You can add between 1 and 25 games using the method.",inline=False)
+    embeds[0].add_field(name="Quick Notify", value="To all of your active public games, use '/quicknotify'. The first time you use this, you may be prompted to use /setdefault. Once you have done this, then the bot will search through all public games and notify you when any of them are waiting for you.",inline=False)
 
     embeds[1].add_field(name="Advanced Commands",value="""There are currently 7 commands:
                           /notify [gameurl]: adds a new notification
