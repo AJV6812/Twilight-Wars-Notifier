0fff5dad8a9d9c26821a9cb18e3d48b96a0863e7
AJV6812 <82735267+AJV6812@users.noreply.github.com>
2022-08-19 16:36:50 +1000

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ async def findgames(session,gameids,playerid, usercount=0,lastGameId = None):
         
     for game in response["games"]:
         for player in game["players"]:
-            if "_id" in player["user"].keys()
+            if "_id" in player["user"].keys():
                 if playerid in player["user"]["_id"]:
                     gameids.append(game)
                 
